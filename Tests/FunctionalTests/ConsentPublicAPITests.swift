a27303aa78ddc721b8e6fe7863f0d5d7f7aaad17
Nick Porter <nporter@adobe.com>
2021-02-08 15:36:23 -0700

    format

--- Tests/FunctionalTests/ConsentPublicAPITests.swift
+++ Tests/FunctionalTests/ConsentPublicAPITests.swift
@@ -10,58 +10,59 @@
  governing permissions and limitations under the License.
  */
 
-@testable import AEPCore
 @testable import AEPConsent
+@testable import AEPCore
 import XCTest
 
 class ConsentPublicAPITests: XCTestCase {
-    
+
     override func setUp() {
         EventHub.reset()
         MockExtension.reset()
         EventHub.shared.start()
         registerMockExtension(MockExtension.self)
     }
-    
+
     private func registerMockExtension<T: Extension> (_ type: T.Type) {
         let semaphore = DispatchSemaphore(value: 0)
-        EventHub.shared.registerExtension(type) { (error) in
+        EventHub.shared.registerExtension(type) { _ in
             semaphore.signal()
         }
 
         semaphore.wait()
     }
-    
+
     // MARK: getConsents(...) test
-    
+
     /// Ensures that the get consent API dispatches the correct event
     func testGetConsents() {
         // setup
         let expectation = XCTestExpectation(description: "getConsents should dispatch an event")
         expectation.assertForOverFulfill = true
-        EventHub.shared.getExtensionContainer(MockExtension.self)?.registerListener(type: EventType.consent, source: "com.adobe.eventSource.requestConsent") { (event) in
+        EventHub.shared.getExtensionContainer(MockExtension.self)?.registerListener(type: EventType.consent, source: "com.adobe.eventSource.requestConsent") { _ in
             expectation.fulfill()
         }
 
         // test
-        Consent.getConsents { (consents, error) in
+        Consent.getConsents { _, _ in
             // nothing
         }
 
         // verify
         wait(for: [expectation], timeout: 1)
     }
-    
+
     // MARK: updateConsents(...) test
-    
+
+    /// Ensures that update consents API dispatches the correct event with correct event data
     func testUpdateConsents() {
         // setup
         let consents = Consents()
         consents.collect = ConsentValue(.yes)
-        
+
         let expectation = XCTestExpectation(description: "updateConsents should dispatch an event with correct payload")
         expectation.assertForOverFulfill = true
-        EventHub.shared.getExtensionContainer(MockExtension.self)?.registerListener(type: EventType.consent, source: "com.adobe.eventSource.updateConsent") { (event) in
+        EventHub.shared.getExtensionContainer(MockExtension.self)?.registerListener(type: EventType.consent, source: "com.adobe.eventSource.updateConsent") { event in
             let dispatchedConsents = ConsentPreferences.from(eventData: event.data!)
             XCTAssertEqual(consents, dispatchedConsents?.consents) // consents in update event should be equal
             expectation.fulfill()
