4646c79437ca09629110e8cb54d3fff30930bb70
Nick Porter <nporter@adobe.com>
2021-02-08 12:34:19 -0700

    Move log

--- Sources/Consent.swift
+++ Sources/Consent.swift
@@ -112,15 +112,15 @@ public class Consent: NSObject, Extension {
     private func dispatchPrivacyOptInIfNeeded(newPreferences: ConsentPreferences) {
         // Only update privacy to opt-in if the new preferences contains "yes" for collect
         guard newPreferences.consents.collect?.val == .yes else { return }
-
+        Log.debug(label: friendlyName,
+                  "New consent preferences contains collect with yes value. Dispatching configuration update event to set privacy status opt-in.")
+        
         let configDict = [ConsentConstants.EventDataKeys.Configuration.GLOBAL_CONFIG_PRIVACY: PrivacyStatus.optedIn.rawValue]
         let event = Event(name: ConsentConstants.EventNames.CONFIGURATION_UPDATE,
                           type: EventType.configuration,
                           source: EventSource.requestContent,
                           data: [ConsentConstants.EventDataKeys.Configuration.UPDATE_CONFIG: configDict])
         dispatch(event: event)
-        Log.debug(label: friendlyName,
-                  "New consent preferences contains collect with yes value. Dispatching configuration update event to set privacy status opt-in.")
     }
 
 }
