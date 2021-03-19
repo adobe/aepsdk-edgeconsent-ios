d8fb41ae9dc717e6df4f05d541fc8fa88afd8d25
Pravin Prakash Kumar <pravinpk0707@gmail.com>
2021-03-19 11:23:36 -0700

    [Dev] - Objective C test app for consentEdge (#26)
    * [Dev]- Objective C test app
    
    * [Dev] - update ObjectiveC testApp
    
    * [Dev] - Strip out unused lines, delegates and remove app warnings

---
+++ TestAppObjc/AppDelegate.m
@@ -0,0 +1,46 @@
+//
+// Copyright 2021 Adobe. All rights reserved.
+// This file is licensed to you under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may obtain a copy
+// of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under
+// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+// OF ANY KIND, either express or implied. See the License for the specific language
+// governing permissions and limitations under the License.
+//
+
+#import "AppDelegate.h"
+
+@import AEPCore;
+@import AEPServices;
+@import AEPEdgeConsent;
+
+@interface AppDelegate ()
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // set defaultConsents
+    NSMutableDictionary *config = [[NSMutableDictionary alloc] init];
+    [config setValue:@{@"consents" : @{ @"collect": @{@"val": @"n"}}} forKey:@"consent.default"];
+    
+    [AEPMobileCore updateConfiguration:config];
+    //[AEPMobileCore configureWithAppId:@"<Your_AppID>"];
+    
+    [AEPMobileCore setLogLevel: AEPLogLevelTrace];
+    [AEPMobileCore registerExtensions:@[AEPMobileEdgeConsent.class] completion:nil];
+    
+    return YES;
+}
+
+#pragma mark - UISceneSession lifecycle
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+@end
